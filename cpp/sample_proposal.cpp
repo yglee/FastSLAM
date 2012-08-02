08bd0f4b197b16c6be0c4d559b42e171ef7328bc
yglee <yeongrace.lee@gmail.com>
2012-08-02 09:25:22 -0700

    switching computers

--- cpp/sample_proposal.cpp
+++ cpp/sample_proposal.cpp
@@ -5,30 +5,41 @@
 //compute proposal distribution, then sample from it, and compute new particle weight
 void sample_proposal(Particle &particle, MatrixXf z, vector<int> idf, MatrixXf R)
 {
-	VectorXf xv = particle.xv();
-	MatrixXf Pv = particle.Pv();
-	VectorXf xv0 = VectorXf(xv);
-	MatrixXf Pv0 = MatrixXf(Pv);	
+		VectorXf xv = particle.xv();
+		MatrixXf Pv = particle.Pv();
+		VectorXf xv0 = VectorXf(xv);
+		MatrixXf Pv0 = MatrixXf(Pv);	
+
+		MatrixXf *Hv;
+		MatrixXf *Hf;
+		MatrixXf *Sf;
+
+		MatrixXf zpi;
+		MatrixXf Hvi;
+		MatrixXf Hfi;
+		MatrixXf Sfi;
+
+		MatrixXf vi (z.rows(),1);
+
+		//process each feature, incrementally refine proposal distribution
+		for (int i =0; i<idf.size(); i++) {
+			vector<int> j;
+			j.push_back(idf[i]);
+			compute_jacobians(particle,j,R,zpi,Hv,Hf,Sf);
+			MatrixXf Sfi = Sf.inverse();
+			Hvi = Hv[0];
+			Hfi = Hf[0];
+			Sfi = Sf[0];
+			
+			vi = z.conservativeResize(z.rows(),1) - zpi;
+			vi(1,0) = pi_to_pi(vi(1,0)); 		
 		
-        MatrixXf zp(z.rows(),idf.size());
-        MatrixXf *Hv;
-        MatrixXf *Hf;
-        MatrixXf *Sf;
-        
-        MatrixXf zpi;
-        MatrixXf Hvi;
-        MatrixXf Hfi;
-        MatrixXf Sfi;
-        
-	//process each feature, incrementally refine proposal distribution
-        for (int i =0; i<idf.size(); i++) {
-		vector<int> j;
-                j.push_back(idf[i]);
-		compute_jacobians(particle,j,R,zp,Hv,Hf,Sf);
-                MatrixXf Sfi = Sf.inverse();
-                zpi = zp[0];
-                Hvi = Hv[0];
-                Hfi = Hf[0];
-                Sfi = Sf[0];
-	}
+			//proposal covariance
+			Pv = Hvi.transpose() * Sfi * Hvi + Pv.inverse();
+			Pv = Pv.inverse();
+				 	
+			//proposal mean
+			
+		}
+		
 }  
