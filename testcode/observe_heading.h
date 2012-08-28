43303ceb677957d5cefcf3abadfa407cbc1fb757
Yeon Jin Lee <grace.lee@nasa.gov>
2012-08-27 19:21:21 -0700

    testcode dir removed

--- testcode/observe_heading.h
+++
@@ -1,9 +0,0 @@
-#ifndef OBSERVE_HEADING_H
-#define OBSERVE_HEADING_H
-
-#include<Eigen/Dense>
-#include "particle.h"
-
-void observe_heading(Particle &particle, float phi, int useheading);
-
-#endif
